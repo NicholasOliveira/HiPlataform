2a932d30ca7c88f162d5bcc39724de175e277020
Nicholas Oliveira <nicholas@atomocom.com.br>
2020-10-10 02:23:38 -0300

    versao inicial

---
+++ build/precache-manifest.82f3e92aff4c51be74b39b31ef20a518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83670aa87595f94cc44627b58b9c72e9",
+    "url": "/HiPlataform/index.html"
+  },
+  {
+    "revision": "0a8e84740e5e15b8e5f2",
+    "url": "/HiPlataform/static/css/main.31af2b55.chunk.css"
+  },
+  {
+    "revision": "48ab5ff458fa52cb2cb9",
+    "url": "/HiPlataform/static/js/2.bd346872.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HiPlataform/static/js/2.bd346872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8e84740e5e15b8e5f2",
+    "url": "/HiPlataform/static/js/main.5a1abbf9.chunk.js"
+  },
+  {
+    "revision": "a8c0e29ee80df2e5a018",
+    "url": "/HiPlataform/static/js/runtime-main.b6200c91.js"
+  }
+]);
